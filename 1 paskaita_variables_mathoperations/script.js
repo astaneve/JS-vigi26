6dea60bcf450f6e2b579383145bb621f594fde59
astaneve <asta.ilgaude@gmail.com>
2022-04-26 20:11:41 +0300

    Add script files

---
+++ 1 paskaita_variables_mathoperations/script.js
@@ -0,0 +1,14 @@
+// One line komentaras
+
+/*
+    Sveiki, cia yra    
+    Multi line komentaras
+*/
+
+// Sukuriam kintamuosius
+let myName;
+let age;
+
+// Priskiriam kintamiesiems reiksmes
+myName = "As Asta";
+age = 43;
