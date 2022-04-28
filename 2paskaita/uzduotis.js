053b114a4f02c2b5ee747c4dc7b09c13dc4813de
astaneve <asta.ilgaude@gmail.com>
2022-04-28 21:20:04 +0300

    Add exercises

---
+++ 2paskaita/uzduotis.js
@@ -0,0 +1,42 @@
+// 1 pratimas
+
+// const legalAge = 20;
+// const clientAge = 13;
+
+// if (clientAge >= legalAge) {
+//     console.log("Klientas yra pilnametis");
+// } else if (clientAge <= legalAge) {
+//     console.log("Klientas yra nepilnametis");
+// }
+
+// 2 pratimas
+
+const name = "Asta";
+
+if (name.length > 6) {
+    console.log("Tavo vardas ilgas");
+}
+
+// 3 pratimas
+const age = 43;
+
+if (age > 100 || age < 0) {
+    console.log("Invalid age");
+} else if (age < 18) {
+    console.log("Child");
+} else {
+    console.log("Adult");
+}
+
+// 4 pratimas
+
+const car = "VW";
+
+
+if (car === "VW" || car === "Bentley" || car === "Audi" || car === "Bugatti" || car === "Lamborghini" || car === "Porsche") {
+    console.log("VW group");
+} else if (car === "BMW" || car === "Mini" || car === "Rolls-Royce") {
+    console.log("BMW group");
+} else {
+    console.log("none group");
+}
