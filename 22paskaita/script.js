3afcc07d64f57e23f504a5fd744cd90ef8294ee5
astaneve <asta.ilgaude@gmail.com>
2022-06-02 20:01:51 +0300

    Add promises

---
+++ 22paskaita/script.js
@@ -0,0 +1,86 @@
+// const networkRequest = () => {
+//     setTimeout(() => {
+//         console.log("Async Code");
+//     }, 4000);
+// };
+
+// console.log("Hello World");
+// networkRequest();
+// console.log("The End");
+
+
+
+// Promises
+
+// const resolvedPromise = new Promise((resolve, reject) => [
+// resolve("17 celsius, Vilnius")
+// ]);
+
+// resolvedPromise.then(response => {
+//     console.log('Dabar yra ${response}');
+// });
+// console.log(promise);
+
+// const rejectedPromise = new Promise((resolve, reject) => {
+//     reject("Tinkalas yraperkrautas. Pabandykite veliau");
+// });
+
+// rejectedPromise.then((response) => {
+//     console.log('Dabar yra ${response}');
+// }).catch(error => {
+//     console.error(error)
+// ;}).finally(() => {
+//     console.log("Kreipimasis i oro tarnyba");
+// });
+
+
+// promise su HTML
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve("17 celsius, Vilnius");
+      reject("Tinklas yra perkrautas. Pabandykite veliau");
+    }, 4000);
+  });
+ 
+  const renderWeather = (response) => {
+    const weather = document.getElementById("weather");
+    weather.textContent = `Dabar yra ${response}`;
+  };
+ 
+  const showError = (err) => {
+    const error = document.getElementById("error");
+    error.textContent = err;
+    error.style.color = "red";
+  };
+ 
+  const showPriceMessage = () => {
+    const header = document.getElementById("header");
+    header.textContent = "Jus pasinaudojote oru paslauga. Jums tai kainavo 5ct";
+  };
+ 
+  const getWeather = async () => {
+    try {
+      const response = await promise;
+      renderWeather(response);
+    } catch (error) {
+      showError(error);
+    } finally {
+      showPriceMessage();
+    }
+  };
+ 
+  getWeather();
+ 
+  // async function getWeatherFunc() {}
+ 
+  // promise
+  //   .then((response) => {
+  //     renderWeather(response);
+  //   })
+  //   .catch((error) => {
+  //     showError(error);
+  //   })
+  //   .finally(() => {
+  //     showPriceMessage();
+  //   });
