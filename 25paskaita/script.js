e6182f46ce4788289e56415940f31bdd24794ab5
astaneve <asta.ilgaude@gmail.com>
2022-06-14 19:42:58 +0300

    Add POST Todo

--- 25paskaita/script.js
+++ 25paskaita/script.js
@@ -17,7 +17,7 @@ document.querySelector(".todo-container").prepend(todoEl);
 fetch("https://jsonplaceholder.typicode.com/todos")
 .then((resp) => resp.json())
 .then((response) => {
-    const todos = response.slice(0, 10);
+    const todos = response.slice(0, 5);
     
 
     todos.forEach((todo) => renderTodo(todo));
@@ -25,3 +25,43 @@ fetch("https://jsonplaceholder.typicode.com/todos")
 .catch((error) => {
     console.error(error);
 });
+
+// issitraukti mygtuka
+// priskirti addEvenet listener
+// issitraukti funkcijos viduje input lauka
+// input duomenis append i todo lista
+
+
+const handleAddTodo = (e) => {
+    const todoText = document.querySelector('input[name="todo-text"]')
+    // console.log(todoText);
+    // console.log("Paspaudziu add mygtuka");
+    const newTodo = {title: todoText.value, completed: false };
+    renderTodo(newTodo);
+
+    const params = {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+headers: {
+    "Content-type": "application.json; charset=UTF-8",
+},
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/todos", params)
+    .then((resp) => resp.json())
+.then((response) => {
+    console.log(response);
+    renderTodo(newTodo);
+    todoText.value = "";
+    console.log(response);
+    
+    })
+.catch((error) => {
+    console.error(error);
+})
+    };
+
+const addButton = document.querySelector(".add-button");
+// console.log(addButton);
+addButton.addEventListener("click", handleAddTodo);
+
